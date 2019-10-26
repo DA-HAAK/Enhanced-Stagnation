1eed5f6b8f0803e27651b6cb54e55ea78ee9bf10
Alicia Koogler <55557067+AliciaK619@users.noreply.github.com>
2019-10-26 18:33:29 -0500

    Create Position.cpp

---
+++ Position.cpp
@@ -0,0 +1,19 @@
+#include "Position.h"
+
+Position::Position()
+{
+  //ctor
+  row = 0;
+  col = 0;
+}
+
+Position::Position(int r, int c){row = r; col = c;}
+
+Position::unsigned int getrow() { return row; }
+
+Position:: unsigned int getcol() { return col; }
+
+Position::~Position()
+{
+  //dtor
+}
