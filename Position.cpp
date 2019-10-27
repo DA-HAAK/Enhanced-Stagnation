50578511d53e054c46a095bc8e81e59cc7b1ebf6
Alicia Koogler <55557067+AliciaK619@users.noreply.github.com>
2019-10-27 02:36:53 -0500

    Update Position.cpp

--- Position.cpp
+++ Position.cpp
@@ -6,18 +6,33 @@ Position::Position()
   row = 0;
   col = 0;
 }
-
+/**
+get the position of the character
+@param r the row
+@param c the column
+*/
 Position::Position(int r, int c){row = r; col = c;}
-
+/**
+get the row of the character
+@return the row
+*/
 Position::unsigned int getrow() { return row; }
-
+/**
+get the column of the character
+@return the column
+*/
 Position:: unsigned int getcol() { return col; }
 
 Position::~Position()
 {
   //dtor
 }
+/**
+shift the row and column
+@param r the row
+@param c the column
+*/
 Position::void shift(int r, int c) {
-    row +=r;
-    col+=c;
+    row += r;
+    col += c;
 }
