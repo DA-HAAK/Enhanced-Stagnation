d4d25bf11cf5473cfd37e02568a29cea1a144c0b
Ashlyn Hanson <44590338+AshlynHanson@users.noreply.github.com>
2019-10-26 23:55:31 -0500

    Add files via upload

---
+++ Hance.cpp
@@ -0,0 +1,45 @@
+#include "Hance.h"
+
+Hance::Hance()
+{
+    difficulty = 1;
+    speed = 1;
+    health = 1;
+    pos = new Position(13, 8);
+    dir = getRandomDir(1,4);
+}
+
+Hance::~Hance()
+{
+    //dtor
+}
+Hance::void wander() {
+    dir = getRandomDir(1,4);
+    if (dir == 'w' && isValidPos(0,-1)) {pos.shift(0,-1);}
+    else if (dir == 's' && is ValidPos(1,0)) {pos.shift(1,0);}
+    else if (dir == 'd' && is ValidPos(0,1)) {pos.shift(0,1);}
+    else if (dir == 'a' && is ValidPos(0,-1)) {pos.shift(0,-1)}
+}
+
+Hance::char getRandomDir(int minimum, int maximum) {
+    int rand = (rand() % maximum) + minimun;
+    if(rand == 1) {return 'w';}
+    else if(rand == 2) {return 's';}
+    else if(rand == 3) {return 'd';}
+    else if(rand == 4) {return 'a';}
+    return;//might be bad
+}
+Hance::bool isValidPos(int r, int c) {
+    if (r == -1 && pos.getRow()==1) {return false;}
+    else if (r == 1 && pos.getRow() ==13) {return false;}
+    else if (c== -1 && pos.getCol() == 1) {return false;}
+    else if (c==1 && pos.getCol() == 13) {return false;}
+    else {return true;}
+}
+Hance::char getId() {
+    return 'H';
+}
+Hence::bool isLocated(Position p) {
+    if (pos == p) {return true;}
+}
+
---
+++ Hance.h
@@ -0,0 +1,25 @@
+#ifndef HANCE_H
+#define HANCE_H
+
+
+class Hance
+{
+    public:
+        Hance();
+        virtual ~Hance();
+        void wander();
+        char getRandomDir(int minimum, int maximum);
+        bool isValidPos(int r, int c);
+        char getId();
+        bool isLocated(Position p);
+    protected:
+
+    private:
+        int speed;
+        int health;
+        Position pos;
+        char dir;
+        int difficulty;
+};
+
+#endif // HANCE_H
