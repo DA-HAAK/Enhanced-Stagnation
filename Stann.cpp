7b3f0e0bc5e1ae4bc3dda7de6128faa4f6606183
Alicia Koogler <55557067+AliciaK619@users.noreply.github.com>
2019-10-26 21:51:32 -0500

    Update Stann.cpp

--- Stann.cpp
+++ Stann.cpp
@@ -29,16 +29,29 @@ gets the health of Stann
 @return the status of Stann's health
 */
 Stann::bool getHealth() {return alive;}
-
+/**
+deals damage to Stann's health
+@return Stann's health, but lessened by one
+*/
 Stann::void healthDown() {health--;}
-
+/**
+changes the position of Stann
+@param dir the direction that Stann will go
+*/
+// if this doesn't work, it is the else statement at the end that is causing problems probably
 Stann::void changePos(char dir) {
     if (dir == 'w' && isValidPos(0,-1)) {row--;}
     else if (dir == 's' && is ValidPos(1,0)) {row++;}
     else if (dir == 'd' && is ValidPos(0,1)) {col++;}
     else if (dir == 'a' && is ValidPos(0,-1)) {col++;}
+    else {return}
 }
-
+/**
+finds if the direction gone is valid or not
+@param r the row where Stann would go
+@param c the row where Stann would go
+@return whether or not Stann can be located at the coordinates
+*/
 Stann::bool isValidPos(int r, int c) {
     if (r == -1 && pos.getRow()==1) {return false;}
     else if (r == 1 && pos.getRow() ==13) {return false;}
