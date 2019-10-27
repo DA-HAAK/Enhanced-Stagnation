f85f5ccaef0067f58e52caf557e31d71a288a7c9
Alicia Koogler <55557067+AliciaK619@users.noreply.github.com>
2019-10-26 23:38:15 -0500

    Update Game.h

--- Game.h
+++ Game.h
@@ -10,7 +10,18 @@ class Game
     public:
         Game();
         virtual ~Game();
+        /**
+        gets the status of the game (playing or finished)
+        @return the status of the game
+        */
         bool getStatus();
+        /**
+        increases the difficulty via adding a hance to the playing field
+        */
+        void increaseDifficulty();
+        /**
+        
+        */
         void setUpPlayer();
         void setUpHance();
         void updateStann(char dir);
