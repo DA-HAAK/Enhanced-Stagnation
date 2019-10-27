6746464a8faa445eb6b0ee306206b54dee7a3c3d
Alicia Koogler <55557067+AliciaK619@users.noreply.github.com>
2019-10-27 01:26:03 -0500

    Update PlayGame.cpp

--- PlayGame.cpp
+++ PlayGame.cpp
@@ -8,31 +8,34 @@ int main() {
     cout << "///////////////////////////////////////////////////////" << endl <<
             "            Welcome to Enhanced Stagnation             " << endl <<
             "///////////////////////////////////////////////////////" << endl <<
-            endl <<
-            "Controls:" << endl <<
-            "             [w]       =           [up]                " << endl <<
-            "         [a] [s] [d]   =   [left] [down] [right]       " << endl <<
-            endl <<
-            "Press any other keys to quit."
-            "-------------------------------------------------------" << endl <<
-            "Goal:" << endl <<
-            "Survive as long as you can." << endl <<
-            "-------------------------------------------------------" << endl <<
-            "Gameplay:" << endl <<
-            "The player Stann ( a variant of 'v', '<', '>', or '^' )" << endl <<
-            "can go in any direction for one space per turn, but the" << endl <<
-            "same goes for the Hance, who are innumerable and have  " << endl <<
-            "decided to make Stann their next meal. They are the    " << endl <<
-            "enhanced, for they can steadily create more of their   " << endl <<
-            "ilk, while stand-still Stann is stuck in the stagnation" << endl <<
-            "of being unable to heal or increase in number. But he  " << endl <<
-            "still stands steadfast and fights to stay alive, his   " << endl <<
-            "point being an impenetrable sheild, impervious to the  " << endl <<
-            "damage that the Hance try to burden him with.          " << endl <<
-            endl <<
-            "See how long you can make Stann survive this onslaught."
-            endl <<
             endl;
+    cout << "Controls:" << endl <<
+            endl <<
+             "             [w]       =           [up]                " << endl <<
+             "         [a] [s] [d]   =   [left] [down] [right]       " << endl <<
+            endl;
+     cout << "Press any other keys to quit." << endl <<
+             "-------------------------------------------------------" << endl <<
+             "Goal:" << endl <<
+             endl <<
+             "Survive as long as you can." << endl <<
+              "-------------------------------------------------------" << endl;
+     cout << "Gameplay:" << endl <<
+             endl <<
+             "The player Stann ( a variant of 'v', '<', '>', or '^' )" << endl <<
+             "can go in any direction for one space per turn, but the" << endl <<
+             "same goes for the Hance, who are innumerable and have  " << endl <<
+             "decided to make Stann their next meal. They are the    " << endl <<
+             "enhanced, for they can steadily create more of their   " << endl <<
+             "ilk, while stand-still Stann is stuck in the stagnation" << endl <<
+             "of being unable to heal or increase in number. But he  " << endl <<
+             "still stands steadfast and fights to stay alive, his   " << endl <<
+             "point being an impenetrable sheild, impervious to the  " << endl <<
+             "damage that the Hance try to burden him with.          " << endl;
+     cout << endl <<
+             "See how long you can make Stann survive this onslaught." <<
+             endl <<
+             endl;
             
     
 
