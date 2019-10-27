73e4c1d4533d214eb54219dd3cd0f353df07fa5f
Ashlyn Hanson <44590338+AshlynHanson@users.noreply.github.com>
2019-10-27 08:45:10 -0500

    Update Stann.cpp

--- Stann.cpp
+++ Stann.cpp
@@ -5,7 +5,7 @@ Stann::Stann()
     alive = true;
     health = 3;
     speed = 1;
-    pos = new Position(1,1);
+    pos = *new Position(1,1);
     dir = 'd';
 
 }
@@ -18,7 +18,7 @@ Stann::~Stann()
 returns the ID for Stann to be printed to the screen
 @return the char ID for Stann
 */
-Stann::char getID() {
+char Stann::getID() {
     if (dir == 'd') {return '>';}
     else if (dir == 's') {return 'v';}
     else if (dir == 'a') {return '<';}
@@ -28,7 +28,7 @@ Stann::char getID() {
 gets the health of Stann
 @return the status of Stann's health
 */
-Stann::bool getStatus() {
+bool Stann::getStatus() {
     if (health > 0) {alive = true;}
     else { alive = false;}
     return alive;
@@ -36,13 +36,13 @@ Stann::bool getStatus() {
 deals damage to Stann's health
 @return Stann's health, but lessened by one
 */
-Stann::void healthDown() {health--;}
+void Stann::healthDown() {health--;}
 /**
 changes the position of Stann
 @param dir the direction that Stann will go
 */
 // if this doesn't work, it is the else statement at the end that is causing problems probably
-Stann::void changePos(char dir) {
+void Stann::changePos(char dir) {
     if (dir == 'w' && isValidPos(0,-1)) {pos.shift(0,-1);}
     else if (dir == 's' && is ValidPos(1,0)) {pos.shift(1,0);}
     else if (dir == 'd' && is ValidPos(0,1)) {pos.shift(0,1);}
@@ -55,7 +55,7 @@ finds if the direction gone is valid or not
 @param c the row where Stann would go
 @return whether or not Stann can be located at the coordinates
 */
-Stann::bool isValidPos(int r, int c) {
+bool Stann::isValidPos(int r, int c) {
     if (r == -1 && pos.getRow() == 1) {return false;}
     else if (r == 1 && pos.getRow() == 13) {return false;}
     else if (c== -1 && pos.getCol() == 1) {return false;}
@@ -68,11 +68,11 @@ checks to see if logic is true
 @param p the position
 @return whether the logic is true
 */
-Stann::bool isLocated(Position p) {
+bool Stann::isLocated(Position p) {
     if (pos == p) {return true;}
 }
 /**
 gets the direction
 @return the direction
 */
-Stann::char getDir() {return dir;}
+char Stann::getDir() {return dir;}
