5aed048382363b0dc0cf7e17ea4f10849574db59
Alicia Koogler <55557067+AliciaK619@users.noreply.github.com>
2019-10-27 02:14:58 -0500

    Update Hance.h

--- Hance.h
+++ Hance.h
@@ -12,11 +12,38 @@ class Hance
     public:
         Hance();
         virtual ~Hance();
+        /**
+        get the direction that the Hance will go
+        */
         void wander();
-        char getRandomDir(int minimum, int maximum);
+        /**
+        get a random direction for the Hance to go
+        @param minimum the minimum random value
+        @param maximum the maximum random value
+        @return a char that corresponds to a number that tells the direction the Hance might go 
+        */
+        char getRandomDir(unsigned minimum, unsigned maximum);
+        /**
+        find if the way the Hance wants to go is valid
+        @param r the row that the Hance will land
+        @param c the column that the Hance will land
+        @return whether or not the path is valid
+        */
         bool isValidPos(int r, int c);
+        /**
+        get the character for the Hance that will be displayed
+        @return the letter 'H'
+        */
         char getId();
+        /**
+        find if position is logical
+        @return whether or not it makes sense
+        */
         bool isLocated(Position p);
+        /**
+        get the direction in which a characer is heading
+        @return the direction in a char value
+        */
         char getDir();
     protected:
 
