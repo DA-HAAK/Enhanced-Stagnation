a63f852ee37cd3cfb5c7afa44dae7de8d3abf3c2
Ashlyn Hanson <44590338+AshlynHanson@users.noreply.github.com>
2019-10-27 08:43:04 -0500

    Update Position.cpp

--- Position.cpp
+++ Position.cpp
@@ -11,17 +11,17 @@ get the position of the character
 @param r the row
 @param c the column
 */
-Position::Position(int r, int c){row = r; col = c;}
+Position::Position(int r, int c) {row = r; col = c;}
 /**
 get the row of the character
 @return the row
 */
-Position::unsigned int getrow() { return row; }
+unsigned int Position::getRow() { return row; }
 /**
 get the column of the character
 @return the column
 */
-Position:: unsigned int getcol() { return col; }
+unsigned int Position::getCol() { return col; }
 
 Position::~Position()
 {
@@ -32,7 +32,7 @@ shift the row and column
 @param r the row
 @param c the column
 */
-Position::void shift(int r, int c) {
+void Position::shift(int r, int c) {
     row += r;
     col += c;
 }
