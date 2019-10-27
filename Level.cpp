a1bc737fbbaf6d72f3ca0b0a34952249766303e3
Alicia Koogler <55557067+AliciaK619@users.noreply.github.com>
2019-10-27 02:24:01 -0500

    Update Level.cpp

--- Level.cpp
+++ Level.cpp
@@ -14,8 +14,14 @@ Level::~Level()
 {
     //dtor
 }
+/**
+get the status of the player
+@return whether the player is alive or not
+*/
 Level::bool getStatus() {return player.getStatus();}
-
+/**
+add a hance to the level
+*/
 Level::void addHance() {
     Hance h = new Hance();
     hances.push_back(h);
@@ -23,9 +29,11 @@ Level::void addHance() {
         levelOne[13][8] = hances.at(i);
         break;
     }
-
 }
-
+/**
+add Stann to the level
+@param player the player Stann
+*/
 Level::void addStann(Stann player) {
     levelOne[1][1] = player;
     for (int i = 0; i< 15;i++ ) {
@@ -36,22 +44,24 @@ Level::void addStann(Stann player) {
         }
     }
 }
-
+/**
+display the game
+*/
 Level::void displayGame() {
 
-    for(int i = 0;i<15;i++) {
-        for (int j=0;j<10;j++) {
+    for(int i = 0; i < 15; i++) {
+        for (int j=0; j < 10; j++) {
             if (player.isLocated(levelOne[i][j])) {
-                cout<<player.getId();
+                cout << player.getId();
             }
             for (size_t k = 0; k<hances.size();k++) {
                 if (hances.at(k).isLocated(levelOne[i][j])) {
-                    cout<<hance.getId();
+                    cout << hance.getId();
                 }
             }
-            cout<<levelOne[i][j]<<endl;
-            cout<<endl;
-            cout<<"Stann's position: ";
+            cout << levelOne[i][j] << endl;
+            cout << endl;
+            cout << "Stann's position: ";
         }
     }
 }
